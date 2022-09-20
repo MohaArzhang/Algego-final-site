f70a353988104e689acf1f8d7ea53b09c8941001
Moha Arzhang <marzhang@gmail.com>
2022-09-20 14:12:33 -0400

    Solving problem of loading menu in 404 page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,9 +1,13 @@
-import Navigation from "./NavigationMenu"
+import NavigationMenu from "./NavigationMenu"
+import React, { useState } from 'react'
+
 
 export default function NotFoundPage() {
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
         <>
-            <Navigation />
+            <NavigationMenu showMenu={showMenu} setShowMenu={setShowMenu} />
             <div className="error">
                 <h1>Erreur 404</h1>
                 <h1>La page que vous avez demandée n'a pas été trouvée</h1>
